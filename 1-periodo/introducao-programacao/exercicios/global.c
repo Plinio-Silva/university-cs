f1ea6ed951ff91163655d3ce381bd5370e5032ff
Plínio Tiago da Silva <pliniotiagos@gmail.com>
2026-05-11 09:14:41 -0400

    inserção de variável local

--- 1-periodo/introducao-programacao/exercicios/global.c
+++ 1-periodo/introducao-programacao/exercicios/global.c
@@ -9,10 +9,15 @@ void funcao() {
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
+
+    int a = 20;
+    printf("Valor da variável local: %d\n", a);
     printf("Valor da variável global: %d\n", global);
 
-    global = 20; // Modificando o valor da variável global
+    global = 5; // Modificando o valor da variável global
     funcao();
+
+    printf("Valor da variável global após modificação: %d\n", global);
 
     getchar(); // Aguarda o usuário pressionar Enter antes de encerrar o programa
     return 0;
