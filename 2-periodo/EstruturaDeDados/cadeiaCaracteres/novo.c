5e8cc05935ff37d39596f5132a6731103e693ec9
Plínio Tiago da Silva <pliniotiagos@gmail.com>
2026-09-01 20:04:53 -0400

    exercitando struck

--- 2-periodo/EstruturaDeDados/cadeiaCaracteres/novo.c
+++ 2-periodo/EstruturaDeDados/cadeiaCaracteres/novo.c
@@ -1,17 +1,55 @@
+// usando struct para armazenar uma cadeia de caracteres
+
 # include <stdio.h>
 # include <locale.h>
 # include <string.h>
 # include <stdlib.h>
 
+typedef struct {
+    char nome[50];
+    char estadoCivil[20];
+    char sexo[10];
+    struct {
+        int dia;
+        char mes[20];
+        int ano;
+    } dataNascimento;
+} Pessoa;
+
 int main() {
     setlocale(LC_ALL, ".utf8");
-    char nome[50];
-    printf("Digite o nome: ");
-    fgets(nome, 50, stdin);
+    Pessoa p1;
+    printf("Digite o nome da pessoa: ");
+    fgets(p1.nome, 50, stdin);
     // Remove the newline character from the end of the string
-    nome[strcspn(nome, "\n")] = 0;
-    printf("O nome digitado foi: %s\n", nome);
+    p1.nome[strcspn(p1.nome, "\n")] = 0;
+    fflush(stdin);
+    printf("Digite estado civil da pessoa: ");
+    fgets(p1.estadoCivil, 20, stdin);
+    // Remove the newline character from the end of the string
+    p1.estadoCivil[strcspn(p1.estadoCivil, "\n")] = 0;
+    fflush(stdin);
+    printf("Digite o sexo da pessoa: ");
+    fgets(p1.sexo, 10, stdin);
+    // Remove the newline character from the end of the string
+    p1.sexo[strcspn(p1.sexo, "\n")] = 0;
+    fflush(stdin);
+    printf("Digite o dia de nascimento da pessoa: ");
+    scanf("%d", &p1.dataNascimento.dia);
+    getchar();
+    printf("Digite o mês de nascimento: ");
+    fgets(p1.dataNascimento.mes, 20, stdin);
+    // Remove the newline character from the end of the string
+    p1.dataNascimento.mes[strcspn(p1.dataNascimento.mes, "\n")] = 0;
+    printf("Digite o ano de nascimento: ");
+    scanf("%d", &p1.dataNascimento.ano);
+    printf("Dados da pessoa:\n");
+    printf("Nome: %s\n", p1.nome);
+    printf("Estado Civil: %s\n", p1.estadoCivil);
+    printf("Sexo: %s\n", p1.sexo);
+    printf("Data de Nascimento: %d/%s/%d\n", p1.dataNascimento.dia, p1.dataNascimento.mes, p1.dataNascimento.ano);
     system("pause");
     return 0;
 }
+
 
